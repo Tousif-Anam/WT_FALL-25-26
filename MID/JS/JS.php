2231212f4a8dae0df4b0cb92d9713f2729e21453
Tousif <tousif.tasin2000@gmail.com>
2025-11-08 22:21:32 +0600

    editted

---
+++ MID/JS/JS.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+
+<title> JS Code Practice </title>
+
+</head>
+
+<body>
+    <h1> Welcome to the lab class </h1>
+    <p id ="demo"> Today we will learn js </p>
+    
+<script> 
+document.write("this is js part")
+document.write("hello world")
+console.log("hello guys, I am js from console")
+alert("hello guys i am from alert")
+
+var a= 40;
+var b= 56.56;
+var name = "tasin";
+var is_human= true;
